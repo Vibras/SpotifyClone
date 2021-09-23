3d366c43e22768dbd647987b24ee8f445d88046b
Ricky <rheredia420@gmail.com>
2021-09-22 21:28:54 -0600

    Installed React App/Created Login Page

---
+++ spotify-clone-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      
+
+      {/* Spotify Logo */}
+
+      {/* Login with Spotify button */}
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ spotify-clone-app/src/spotify.js
@@ -0,0 +1,5 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "535f8a4410264ac18a8953e15af6d234";
+const redirectUri = "http://localhost:3000/";
