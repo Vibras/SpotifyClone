113e31c03dca01618319e2f139b1ad1fc1ff0c76
Ricky <rheredia420@gmail.com>
2021-09-23 21:32:11 -0600

    Added login feature to Spotify account.

--- spotify-clone-app/src/spotify.js
+++ spotify-clone-app/src/spotify.js
@@ -1,5 +1,17 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
 export const authEndpoint = "https://accounts.spotify.com/authorize";
+
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "535f8a4410264ac18a8953e15af6d234";
 const redirectUri = "http://localhost:3000/";
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+)}&response_type=token&show_dialog=true`;
